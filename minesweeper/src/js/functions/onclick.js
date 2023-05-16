591fa831ae88bdef2619909a60e906295d41bb18
Asmat1k <dobrovti@mail.ru>
2023-05-16 21:27:00 +0300

    fix: generation fix (column --> row blocks)

--- minesweeper/src/js/functions/onclick.js
+++ minesweeper/src/js/functions/onclick.js
@@ -1,8 +1,25 @@
+/* eslint-disable import/extensions */
+import { matrix } from './generate-matrix.js';
+
+// проверка на бомбу
+function isBomb(row, column) {
+  if (matrix[row][column] === 1) {
+    return true;
+  }
+  return false;
+}
+
 export default function boxClick() {
   const items = document.querySelectorAll('.game__block');
+  const block = [...items];
   items.forEach((item) => {
-    item.addEventListener('click', () => {
-      console.log('click');
+    item.addEventListener('click', (event) => {
+      const index = block.indexOf(event.target);
+      const column = index % 10;
+      const row = Math.trunc(index / 10);
+      // console.log(`${row} and ${column}`);
+      // console.log(`${item.innerHTML}`);
+      item.innerHTML = isBomb(row, column) ? 'X' : '';
     });
   });
 }
