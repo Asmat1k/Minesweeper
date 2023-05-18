6ec3062fb7d76a7d00789d3239267c88ae16089d
Asmat1k <dobrovti@mail.ru>
2023-05-18 18:35:43 +0300

    fix: timer and new matrix set fixed

--- minesweeper/src/js/functions/box-click.js
+++ minesweeper/src/js/functions/box-click.js
@@ -9,10 +9,10 @@ import newGame from './new-game.js';
 import click from './click.js';
 import genMatrix from './generate-matrix.js';
 
-let flagStatus = false;
-
 export default function boxClick() {
+  let flagStatus = false;
   let isMatrixSet = false;
+  let isTimerRun = false;
   const items = document.querySelectorAll('.game__block');
   const flag = document.querySelector('.flag');
   newGame();
@@ -22,16 +22,19 @@ export default function boxClick() {
       if (!item.classList.contains('game__block_opened') && !item.classList.contains('num') && !item.classList.contains('flag')) {
         click(false);
       }
-      // запуск таймера если это не флаг
-      startTimer();
       // получаем координаты в матрице
       const index = Array.from(items).indexOf(event.target);
       const row = Math.trunc(index / 10);
       const column = index % 10;
       // если матрицы еще не сгенерирована
-      if (!isMatrixSet) {
+      if (!isMatrixSet && !item.classList.contains('flag')) {
         genMatrix(10, row, column);
         isMatrixSet = true;
+      }
+      if (!isTimerRun) {
+        // запуск таймера если это не флаг
+        startTimer();
+        isTimerRun = true;
       }
       // если нажали на флаг
       if (item.classList.contains('flag')) {
