cf493d6ba4c51bc5a95777cfe072740438a07999
Asmat1k <dobrovti@mail.ru>
2023-06-11 14:35:37 +0300

    fix: types fix

--- MigrationToTypeScript/src/components/types.ts
+++ MigrationToTypeScript/src/components/types.ts
@@ -1,27 +1,10 @@
 export type Data = DataSources | DataNews | undefined;
 export type DataSources = { status: string; sources: Source } | undefined;
 export type Endpoint = 'sources' | 'everything';
-export type Source = {
-    id: string;
-    name: string;
-    description: string;
-    url: string;
-    category: string;
-    language: string;
-    country: string;
-};
+export type Source = Record<string, string>;
+export type Article = Record<string, string> & { source: Record<string, string> };
 export type DataNews = {
     status: string;
     totalResults: number;
     articles: Article[];
-};
-export type Article = {
-    author: string;
-    content: string;
-    description: string;
-    publishedAt: string;
-    source: Record<string, string>;
-    title: string;
-    url: string;
-    urlToImage: string;
 };
