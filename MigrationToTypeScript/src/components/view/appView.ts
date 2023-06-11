c63e8c27cfd5f1bc7cd873cd7cb2dc91c4ed1bd4
Asmat1k <dobrovti@mail.ru>
2023-06-11 13:51:10 +0300

    feat: appView done

--- MigrationToTypeScript/src/components/view/appView.ts
+++ MigrationToTypeScript/src/components/view/appView.ts
@@ -1,18 +1,23 @@
+import { Article, Data, DataNews, DataSources } from '../types';
 import News from './news/news';
 import Sources from './sources/sources';
 
 export class AppView {
+    public news: News;
+    public sources: Sources;
+
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
-        const values = data?.articles ? data?.articles : [];
+    drawNews(data: DataNews): void {
+        const values: Article[] = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
+    // valuse: type ?
+    drawSources(data: DataSources): void {
         const values = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
