ef4d56e833275ac9e66324f2aa87aefabe269b68
Asmat1k <dobrovti@mail.ru>
2023-06-11 13:58:25 +0300

    feat: sources done

---
+++ MigrationToTypeScript/src/components/view/sources/sources.ts
@@ -0,0 +1,31 @@
+import './sources.css';
+import { Source } from '../../types';
+
+class Sources {
+    draw(data: Source[]): void {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+
+        data.forEach((item) => {
+            if (sourceItemTemp) {
+                const sourceClone: DocumentFragment = sourceItemTemp.content.cloneNode(true) as DocumentFragment;
+                const sourceItemName = sourceClone.querySelector('.source__item-name');
+
+                if (sourceItemName) {
+                    sourceItemName.textContent = item.name;
+                } else {
+                    throw new Error(".source__item-name doesn't found ");
+                }
+
+                sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
+                fragment.append(sourceClone);
+            } else {
+                throw new Error("#sourceItemTemp doesn't found ");
+            }
+        });
+
+        document.querySelector('.sources')?.append(fragment);
+    }
+}
+
+export default Sources;
