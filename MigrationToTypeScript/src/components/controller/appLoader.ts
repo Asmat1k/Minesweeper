1c169a4fbf8195de4ee1527a904d13b2145d5100
Asmat1k <dobrovti@mail.ru>
2023-06-12 23:30:21 +0300

    fix: link fix for api

--- MigrationToTypeScript/src/components/controller/appLoader.ts
+++ MigrationToTypeScript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '23d6d0cc7eea4335aaaaae4f27aeafcd',
         });
     }
