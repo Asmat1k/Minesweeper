f0c9eece6a18a78407d02720035b3af3b59d52a2
Asmat1k <dobrovti@mail.ru>
2023-06-20 14:11:08 +0300

    feat: burger lines rotation added

--- RSS-CSS-Selectors/#src/js/files/burger.js
+++ RSS-CSS-Selectors/#src/js/files/burger.js
@@ -3,10 +3,18 @@ export function menuInit() {
   if (document.querySelector('.nav-burger')) {
     document.addEventListener('click', (event) => {
       if (event.target.closest('.nav-burger')) {
+        lineChange();
         document.documentElement.classList.toggle('menu-open');
         document.querySelector('.nav__header_menu').classList.toggle('menu-open');
       }
     });
+  }
+}
+
+function lineChange() {
+  const lines = document.querySelectorAll('.burger-line');
+  for(let i = 0; i < lines.length; i += 1) {
+    lines[i].classList.toggle('active');
   }
 }
 
